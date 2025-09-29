28c56f89ffa6be088dd1ac7d95bceaf33e979582
srikshith512 <167422013+srikshith512@users.noreply.github.com>
2025-09-29 10:22:01 +0530

    Create remove_element.java

---
+++ remove_element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int slow = 0;
+        for ( int f = 0 ; f < nums.length ; f++){
+            if(nums[f] != val){
+                
+                nums[slow] = nums[f];
+                slow++;
+                
+            }
+        }
+        return slow;
+        
+    }
+}
